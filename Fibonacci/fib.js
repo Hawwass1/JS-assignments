bba558a48da55753ee3bb2e383419e1f303592a5
Hawwass1 <suleimanhawwa.sh@gmail.com>
2022-08-21 23:31:33 +0100

    first commit

---
+++ Fibonacci/fib.js
@@ -0,0 +1,12 @@
+function fibonacci(num){
+    if (num == 0)
+    return 0
+    else if(num == 1 || num == 2)
+    return 1
+    else if (num < 0)
+    return "Input is invalid"
+    else return fibonacci(num -1) + fibonacci (num -2)
+}
+
+console.log(fibonacci(-2))
+console.log(fibonacci(7))
